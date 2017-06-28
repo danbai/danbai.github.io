c3a66279db4c100cf4f38f74aa0698fa2ae35198
jlwang <jlwang@grandstream.com>
2017-06-28 16:28:12 +0800

    nav

--- react-all/react-router/08/components/Nav1.jsx
+++ react-all/react-router/08/components/Nav1.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
 class Nav1 extends Component {
+    _clickBrowserHistory = () => {
+        browserHistory.push('/nav2');
+    }
     render() {
         return (
-            <div>Nav1</div>
+            <div>
+                Nav1
+                <button onClick={ this._clickBrowserHistory }>button</button>
+            </div>
         );
     }
 }
