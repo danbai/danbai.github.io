64bbc04323cf4aac61121525824d808af46449ae
danbai <1006894640@qq.com>
2018-09-29 14:53:03 +0800

    css loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import './styles/index.css';
 
 class App extends Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,6 +14,10 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loaders: ['style-loader', 'css-loader'],
+            include: APP_PATH
         }]
     },
     devtool: 'cheap-module-eval-source-map',
